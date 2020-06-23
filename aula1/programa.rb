5d94a3ad2ffb2700c48b36148e8fad1dac96d8bb
deividyforlin <deividy.forlin@gmail.com>
2020-06-23 12:38:28 +0000

    perguntando se variável nome_completo é nil

--- aula1/programa.rb
+++ aula1/programa.rb
@@ -3,6 +3,7 @@ class Topico
   def programa_metodo_nil
    nome_completo = "Deividy Fritsch"
    puts nome_completo
-   nome_completo = nil?
+   nome_completo = nil
+   puts nome_completo.nil?
   end
 end
