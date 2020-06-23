7a311a6bfaf74c5953b6bbb42c6025609707066e
deividyforlin <deividy.forlin@gmail.com>
2020-06-23 13:37:52 +0000

    usando if e else ( se ou senão )

--- aula1/programa.rb
+++ aula1/programa.rb
@@ -1,11 +1,12 @@
 class Topico
 
-  def programa_operadores
-  comparacao = ">, >=, <, <=, ==, !="
-  puts comparacao
-  logica = "!, &&, ||, not, and, or"
-  puts logica
-  matematicos = "+, -, *, /, %,**"
-  puts matematicos
+  def programa_usando_if_e_end
+    a,b = 1,2
+    #testando if e end
+    if a > b # if = se
+      puts " A é maior que B"
+      else # else = senão
+        puts "B é maior que A"
+      end
   end
 end
--- main.rb
+++ main.rb
@@ -1,4 +1,4 @@
 require_relative "aula1/programa"
 
 #Topico.new.metodo_te
-Topico.new.programa_operadores
+Topico.new.programa_usando_if_e_end
