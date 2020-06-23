5a75b84ef02402a8a297bc4889f38c457000ce2e
deividyforlin <deividy.forlin@gmail.com>
2020-06-23 12:33:23 +0000

    fazendo variável usando método nil

--- aula1/programa.rb
+++ aula1/programa.rb
@@ -1,7 +1,8 @@
 class Topico
 
-  def programa_variaveis_snickers
+  def programa_metodo_nil
    nome_completo = "Deividy Fritsch"
    puts nome_completo
+   nome_completo = nil?
   end
 end
--- main.rb
+++ main.rb
@@ -1,4 +1,4 @@
 require_relative "aula1/programa"
 
 #Topico.new.metodo_te
-Topico.new.programa_variaveis_snickers
+Topico.new.programa_metodo_nil
